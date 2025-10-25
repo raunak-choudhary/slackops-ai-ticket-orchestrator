21aec29fd02d3c06026c12b651f15d8d51323c81
Aayush Anand <aayushanand@Aayushs-MacBook-Air-2.local>
2025-10-25 19:49:21 -0400

    fix: clean imports, add pyproject, remove pytest flags

--- src/gmail_impl/setup.py
+++ src/gmail_impl/setup.py
@@ -1,2 +1,7 @@
 from setuptools import find_packages, setup
-setup(name='gmail_impl', version='0.1.0', packages=find_packages())
+
+setup(
+    name='gmail_impl',
+    version='0.1.0',
+    packages=find_packages()
+)
--- src/mail_client_adapter/setup.py
+++ src/mail_client_adapter/setup.py
@@ -1,2 +1,7 @@
 from setuptools import find_packages, setup
-setup(name='mail_client_adapter', version='0.1.0', packages=find_packages())
+
+setup(
+    name='mail_client_adapter',
+    version='0.1.0',
+    packages=find_packages()
+)
