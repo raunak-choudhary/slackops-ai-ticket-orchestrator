72c9a964bf20bde83196686b71e6cb1f6091192a
Aayush Anand <aayushanand@Aayushs-MacBook-Air-2.local>
2025-10-25 20:10:25 -0400

    fix: add placeholder classes for test imports (AdapterClient, ServiceAdapter, SlackAPI models)

--- src/mail_client_adapter/adapter.py
+++ src/mail_client_adapter/adapter.py
@@ -1,1 +1,3 @@
-
+class AdapterClient: pass
+class ServiceAdapter: pass
+class ServiceBackedClient: pass
---
+++ src/slack_api/__init__.py
@@ -0,0 +1,5 @@
+class Channel: pass
+class ChatClient: pass
+class Message: pass
+class TokenStore: pass
+class User: pass
